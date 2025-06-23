50160d6239d4a6102e606726862be11bed383a3d
R Holder <59735305+alienpoodle@users.noreply.github.com>
2025-06-22 20:32:16 -0400

    fixed Service worker path

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ import {
 // --- Register Service Worker for PWA ---
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then(registration => console.log('ServiceWorker registered'))
             .catch(err => console.log('ServiceWorker registration failed:', err));
     });
